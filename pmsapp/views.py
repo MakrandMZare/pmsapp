8195b7049dbde278048f0285cb830eb065224975
MakrandMZare <makzare1463@gmail.com>
2023-03-07 23:16:40 +0530

    07032023

--- pmsapp/views.py
+++ pmsapp/views.py
@@ -2,6 +2,8 @@ from django.db.models import Count
 from django.shortcuts import render
 from django.views import View
 from . models import Product
+from . forms import CustomerRegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -29,3 +31,16 @@ class ProductDetail(View):
     def get(self,request,pk):
         product = Product.objects.get(pk=pk)
         return render(request,"app/productdetail.html",locals())
+    
+class CustomerRegistrationView(View):
+    def get(self,request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html',locals())
+    def post(self,request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Congratulations! User Register Successfully.")
+        else:
+            messages.warning(request,"Invalid Input Data.")
+            return render(request, 'app/customerregistration.html',locals())
